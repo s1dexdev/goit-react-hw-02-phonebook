65818ac2a6a8e1305635f5317146954a2272a22b
s1dexdev <s1dexdev@gmail.com>
2021-03-14 16:17:13 +0200

    added hooks

--- src/App.js
+++ src/App.js
@@ -1,85 +1,157 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import PropTypes from 'prop-types';
 import ContactForm from './components/ContactForm/ContactForm';
 import Filter from './components/Filter/Filter';
 import ContactList from './components/ContactList/ContactList';
 
-class App extends Component {
-  static propTypes = {
-    contacts: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        number: PropTypes.string.isRequired,
-      }),
-    ),
-    filter: PropTypes.string,
-  };
+//  HOOKS
 
-  state = {
-    contacts: [],
-    filter: '',
-  };
+const App = function () {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  handleFilter = event => {
+  const handleFilter = event => {
     const { value } = event.currentTarget;
 
-    this.setState({ filter: value });
+    setFilter(value);
   };
 
-  handleNewContact = newContact => {
-    const { name, number } = newContact;
-    const isAlreadyInContacts = this.checkIdenticalNames(name);
+  const checkIdenticalNames = name => {
+    return contacts.some(contact => contact.name === name);
+  };
+
+  const addContact = (name, number) => {
+    const contactId = uuidv4();
+    const userInfo = { id: contactId, name, number };
+
+    setContacts(prevState => [...prevState, userInfo]);
+  };
+
+  const deleteContact = contactId => {
+    setContacts(contacts.filter(({ id }) => id !== contactId));
+  };
+
+  const handleNewContact = (name, number) => {
+    const isAlreadyInContacts = checkIdenticalNames(name);
 
     if (isAlreadyInContacts) {
       return alert(`${name} is already in contacts`);
     } else {
-      this.addContact(name, number);
+      addContact(name, number);
     }
   };
 
-  checkIdenticalNames = name => {
-    return this.state.contacts.some(contact => contact.name === name);
-  };
+  const normalizeFilter = filter.toLowerCase();
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizeFilter),
+  );
 
-  addContact = (name, number) => {
-    const contactId = uuidv4();
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={handleNewContact} />
 
-    this.setState(({ contacts }) => {
-      const userInfo = { id: contactId, name, number };
+      <h2>Contacts</h2>
+      <Filter value={filter} onFilter={handleFilter} />
+      <ContactList
+        onDeleteContact={deleteContact}
+        visibleContacts={visibleContacts}
+      />
+    </div>
+  );
+};
 
-      return { contacts: [...contacts, userInfo] };
-    });
-  };
-
-  deleteContact = contactId => {
-    this.setState(({ contacts }) => ({
-      contacts: contacts.filter(({ id }) => id !== contactId),
-    }));
-  };
-
-  render() {
-    const { filter, contacts } = this.state;
-    const normalizeFilter = filter.toLowerCase();
-    const visibleContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizeFilter),
-    );
-
-    return (
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.handleNewContact} />
-
-        <h2>Contacts</h2>
-        <Filter value={filter} onFilter={this.handleFilter} />
-        <ContactList
-          onDeleteContact={this.deleteContact}
-          visibleContacts={visibleContacts}
-        />
-      </div>
-    );
-  }
-}
+App.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }),
+  ),
+  filter: PropTypes.string,
+};
 
 export default App;
+
+//  CLASS
+
+// class App extends Component {
+//   static propTypes = {
+//     contacts: PropTypes.arrayOf(
+//       PropTypes.shape({
+//         id: PropTypes.string.isRequired,
+//         name: PropTypes.string.isRequired,
+//         number: PropTypes.string.isRequired,
+//       }),
+//     ),
+//     filter: PropTypes.string,
+//   };
+
+//   state = {
+//     contacts: [],
+//     filter: '',
+//   };
+
+//   handleFilter = event => {
+//     const { value } = event.currentTarget;
+
+//     this.setState({ filter: value });
+//   };
+
+//   handleNewContact = newContact => {
+//     const { name, number } = newContact;
+//     const isAlreadyInContacts = this.checkIdenticalNames(name);
+
+//     if (isAlreadyInContacts) {
+//       return alert(`${name} is already in contacts`);
+//     } else {
+//       this.addContact(name, number);
+//     }
+//   };
+
+//   checkIdenticalNames = name => {
+//     return this.state.contacts.some(contact => contact.name === name);
+//   };
+
+//   addContact = (name, number) => {
+//     const contactId = uuidv4();
+
+//     this.setState(({ contacts }) => {
+//       const userInfo = { id: contactId, name, number };
+
+//       return { contacts: [...contacts, userInfo] };
+//     });
+//   };
+
+//   deleteContact = contactId => {
+//     this.setState(({ contacts }) => ({
+//       contacts: contacts.filter(({ id }) => id !== contactId),
+//     }));
+//   };
+
+//   render() {
+//     const { filter, contacts } = this.state;
+//     const normalizeFilter = filter.toLowerCase();
+//     const visibleContacts = contacts.filter(contact =>
+//       contact.name.toLowerCase().includes(normalizeFilter),
+//     );
+
+//     return (
+//       <div>
+//         <h1>Phonebook</h1>
+//         <ContactForm onSubmit={this.handleNewContact} />
+
+//         <h2>Contacts</h2>
+//         <Filter value={filter} onFilter={this.handleFilter} />
+//         <ContactList
+//           onDeleteContact={this.deleteContact}
+//           visibleContacts={visibleContacts}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
